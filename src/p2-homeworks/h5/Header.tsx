072928a3ef86674cb79f182f69e3aca7bb3ffc2f
YACHNIKMIKHAIL <limerick2011@yandex.by>
2021-12-10 15:32:38 +0300

    2.3 abyfkjxrf

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -7,10 +7,13 @@ import s from './Header.module.css'
 function Header() {
     return (
         <div className={s.slide}>
-            <NavLink to={PATH.PRE_JUNIOR}>PRE_JUNIOR</NavLink>
-             <NavLink to={PATH.JUNIOR}>JUNIOR</NavLink>
-             <NavLink to={PATH.JUNIORplus}>JUNIORplus</NavLink>
-             <span>show me)</span>
+            <NavLink to={PATH.PRE_JUNIOR}
+                     className={({isActive}) => ((isActive ? s.active : ''))}>PRE_JUNIOR</NavLink>
+            <NavLink to={PATH.JUNIOR}
+                     className={({isActive}) => ((isActive ? s.active : ''))}>JUNIOR</NavLink>
+            <NavLink to={PATH.JUNIORplus}
+                     className={({isActive}) => ((isActive ? s.active : ''))}>JUNIORplus</NavLink>
+            <span>show me)</span>
         </div>
     )
 }
