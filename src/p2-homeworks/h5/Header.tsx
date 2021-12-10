08f9acc30b4de036fff646e288071aede33e869d
YACHNIKMIKHAIL <limerick2011@yandex.by>
2021-12-10 14:50:55 +0300

    2.2 commit first navLink+++

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import {PATH} from "./Routes";
 import {NavLink} from "react-router-dom";
 import s from './Header.module.css'
+import JUNIORplus from "./pages/JUNIORplus";
 
 function Header() {
     return (
         <div className={s.slide}>
             <NavLink to={PATH.PRE_JUNIOR}>PRE_JUNIOR</NavLink>
+             <NavLink to={PATH.JUNIOR}>JUNIOR</NavLink>
+             <NavLink to={PATH.JUNIORplus}>JUNIORplus</NavLink>
+             <span>show me)</span>
         </div>
     )
 }
--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import {Routes, Route, Navigate} from "react-router-dom";
 import PreJunior from "./pages/PreJunior";
+import Junior from "./pages/Junior";
+import JUNIORplus from "./pages/JUNIORplus";
 import Error404 from "./pages/Error404";
 
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
-    // add paths
+    JUNIOR: '/junior',
+    JUNIORplus: '/junior+'
 }
 
 function ROUtes() {
@@ -17,9 +20,14 @@ function ROUtes() {
             {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
             {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
                 <Route path={'PRE_JUNIOR'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
-
                 <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
-                // add routes
+
+                <Route path={'JUNIOR'} element={<Navigate to={PATH.JUNIOR}/>}/>
+                <Route path={PATH.JUNIOR} element={<Junior/>}/>
+
+                <Route path={'JUNIORplus'} element={<Navigate to={PATH.JUNIORplus}/>}/>
+                <Route path={PATH.JUNIORplus} element={<JUNIORplus/>}/>
+
 
             {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
             <Route element={ <Error404/>}/>
