f73fe547bbae4c4b23ff35e5d488a29823e715c4
YACHNIKMIKHAIL <limerick2011@yandex.by>
2021-12-10 14:33:39 +0300

    2.2 commit first navLink

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import {PATH} from "./Routes";
+import {NavLink} from "react-router-dom";
+import s from './Header.module.css'
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        <div className={s.slide}>
+            <NavLink to={PATH.PRE_JUNIOR}>PRE_JUNIOR</NavLink>
         </div>
     )
 }
