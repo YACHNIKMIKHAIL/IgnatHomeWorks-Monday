d6285c0d19f193219e642bc9e0081a283b56e927
YACHNIKMIKHAIL <limerick2011@yandex.by>
2021-12-27 16:02:12 +0300

    troublesss commit

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import HW7 from "../../h7/HW7";
+import HW8 from "../../h8/HW8";
 
 
 function JUNIOR() {
@@ -7,6 +8,7 @@ function JUNIOR() {
         <div>
             JUNIOR
             <HW7/>
+            <HW8/>
         </div>
     )
 }
--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,23 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+export const homeWorkReducer = (state: UserType[], action: { type: 'sort' | 'check', payload: 'up' | 'down' | number }): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': {
             // need to fix
-            return state
+            (action.payload === 'up')
+            {
+                return state.map(m => ({...m})).sort((a, b) => a.name > b.name ? 1 : -1)
+            }
+            (action.payload === 'down')
+            {
+                return state.map(m => ({...m})).sort((a, b) => a.name < b.name ? -1 : 1)
+            }
         }
         case 'check': {
             // need to fix
-            return state
+            return state.filter(f => f.age > action.payload)
         }
-        default: return state
+        default:
+            return state
     }
 }
