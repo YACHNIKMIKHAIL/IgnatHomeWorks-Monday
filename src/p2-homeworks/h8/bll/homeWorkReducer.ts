6ab9c57e751699e790c9ad1b8abb4e312ca3220b
YACHNIKMIKHAIL <limerick2011@yandex.by>
2021-12-27 16:09:32 +0300

    DONE commit

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,21 +1,20 @@
 import {UserType} from "../HW8";
 
-export const homeWorkReducer = (state: UserType[], action: { type: 'sort' | 'check', payload: 'up' | 'down' | number }): UserType[] => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: { type: 'sort' | 'check', payload: 'up' | 'down' | string | number }): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': {
             // need to fix
-            (action.payload === 'up')
-            {
+            if (action.payload === 'up') {
                 return state.map(m => ({...m})).sort((a, b) => a.name > b.name ? 1 : -1)
-            }
-            (action.payload === 'down')
-            {
-                return state.map(m => ({...m})).sort((a, b) => a.name < b.name ? -1 : 1)
+            } else if (action.payload === 'down') {
+                return state.map(m => ({...m})).sort((a, b) => a.name < b.name ? 1 : -1)
+            } else {
+                return {...state}
             }
         }
         case 'check': {
             // need to fix
-            return state.filter(f => f.age > action.payload)
+            return state.filter(f => f.age > +action.payload)
         }
         default:
             return state
