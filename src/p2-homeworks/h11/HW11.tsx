1b03db7df328683cfee1a23c4ba370c286b32d25
YACHNIKMIKHAIL <limerick2011@yandex.by>
2022-01-27 16:53:49 +0300

    half$ucces commit

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -3,27 +3,29 @@ import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
 
 function HW11() {
-    const [value1, setValue1] = useState(0)
-    const [value2, setValue2] = useState(100)
+    const [value1, setValue1] = useState<number>(0)
+    const [value2, setValue2] = useState<number>(100)
+
+    const onChangeRange = (value: number) => {
+        setValue1(value)
+    }
+    const onChangeRangeII = (value: Array<number>) => {
+        setValue1(value[0])
+        setValue2(value[1])
+    }
 
     return (
         <div>
             <hr/>
             homeworks 11
-
-            {/*should work (должно работать)*/}
             <div>
                 <span>{value1}</span>
-                <SuperRange
-                    // сделать так чтоб value1 изменялось
-                />
+                <SuperRange onChangeRange={onChangeRange} value={value1}/>
             </div>
 
             <div>
                 <span>{value1}</span>
-                <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
-                />
+                <SuperDoubleRange onChangeRange={onChangeRangeII}/>
                 <span>{value2}</span>
             </div>
 
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,23 +1,67 @@
-import React from 'react'
+import React, {ChangeEvent} from 'react'
+import {Box, Slider} from "@mui/material";
 
 type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
-    // min, max, step, disable, ...
+    onChangeRange?: (value: Array<number>) => void
+    value?: Array<number>
 }
-
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
-        onChangeRange, value,
-        // min, max, step, disable, ...
+         onChangeRange, value,
+
     }
 ) => {
-    // сделать самому, можно подключать библиотеки
+    //
+    // const [valueX, setValueX] = React.useState<number[]>([20, 37]);
+    //
+    // // const onChangeCallback = (e: ChangeEvent<HTMLInputElement>, newValue: number[]) => {
+    // //     setValueX(newValue)
+    // //     if (onChangeRange) {
+    // //         onChangeRange(newValue);
+    // //     }
+    // // }
+    // const handleChange = (event: Event, newValue: number | number[]) => {
+    //     if (onChangeRange) {
+    //         if (typeof newValue === 'number')
+    //             setValueX([newValue, 0])
+    //         onChangeRange(valueX);
+    //     }
+    // };
+    const [value2, setValue2] = React.useState<number[]>([20, 37]);
+    const minDistance = 10;
+    const handleChange2 = (
+        event: Event,
+        newValue: number | number[],
+        activeThumb: number,
+    ) => {
+        if (!Array.isArray(newValue)) {
+            return;
+        }
+
+        if (newValue[1] - newValue[0] < minDistance) {
+            if (activeThumb === 0) {
+                const clamped = Math.min(newValue[0], 100 - minDistance);
+                setValue2([clamped, clamped + minDistance]);
+            } else {
+                const clamped = Math.max(newValue[1], minDistance);
+                setValue2([clamped - minDistance, clamped]);
+            }
+        } else {
+            setValue2(newValue as number[]);
+        }
+    };
 
     return (
-        <>
-            DoubleRange
-        </>
+        <Box sx={{width: 300}}>
+            <Slider
+                getAriaLabel={() => 'Minimum distance shift'}
+                value={value2}
+                onChange={handleChange2}
+                valueLabelDisplay="auto"
+                // getAriaValueText={valuetext}
+                disableSwap
+            />
+        </Box>
     )
 }
 
